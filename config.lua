cf6cdf00416b7d015e1b633c66c01e7c83e895f2
test8585bs <bamdadnikkhah1@gmail.com>
2017-02-17 02:26:53 -0800

    Update config.lua

--- config.lua
+++ config.lua
@@ -4,7 +4,7 @@ return {
   channel = 'JackiTeam_ch',
 
   plugins = {
-    'Help.lua','Panel.lua','Exam.lua','DataBase.lua'
+    'Help.lua','Panel.lua','Exam.lua','DataBase.lua', 'Start.lua'
   },
   api_errors = {
     [101] = 'Not enough rights to kick participant',
