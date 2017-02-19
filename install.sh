602b1caba431220a6fc2dea7db706b9b32bbd8db
test8585bs <bamdadnikkhah1@gmail.com>
2017-02-19 05:21:42 -0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,13 @@
+git pull
+wget http://luarocks.org/releases/luarocks-2.2.2.tar.gz
+tar zxpf luarocks-2.2.2.tar.gz
+cd luarocks-2.2.2
+./configure; sudo make bootstrap
+sudo luarocks install luasocket
+sudo luarocks install luasec
+sudo luarocks install redis-lua
+sudo luarocks install lua-term
+sudo luarocks install serpent
+sudo apt-get install curl
+sudo service redis-server start
+sudo apt-get install libreadline-dev libconfig-dev libssl-dev lua5.2 liblua5.2-dev lua-socket lua-sec lua-expat libevent-dev make unzip git redis-server autoconf g++ libjansson-dev libpython-dev expat libexpat1-dev
