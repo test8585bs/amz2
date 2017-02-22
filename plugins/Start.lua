59681973d9c1e8aa8cb1e1b1dbef9f4769cedbeb
test8585bs <bamdadnikkhah1@gmail.com>
2017-02-22 11:30:30 +0330

    Add files via upload

--- plugins/Start.lua
+++ plugins/Start.lua
@@ -1,110 +1,142 @@
 local function do_keyboard_private()
-    local keyboard = {}
-    keyboard.inline_keyboard = {
-    	{{text = '?????? ????', callback_data = '!school'}}, {{text = '?????????', callback_data = '!teach'}}}
-    return keyboard
+    local keyboard = {}
+    keyboard.inline_keyboard = {
+    	{
+			{text = 'Ø¯Ø§Ù†Ø´ Ø¢Ù…ÙˆØ²', callback_data = '!school'},
+		},
+		{
+			{text = 'Ø¢Ù…ÙˆØ²Ú¯Ø§Ø±', callback_data = '!teach'},
+		}
+	}
+    return keyboard
 end
-
+local function do_keyboard_privatea()
+    local keyboard = {}
+    keyboard.inline_keyboard = {
+    	{
+			{text = 'Ø¯Ø§Ù†Ø´ Ø¢Ù…ÙˆØ²', callback_data = '!school'},
+		},
+		{
+			{text = 'Ø¢Ù…ÙˆØ²Ú¯Ø§Ø±', callback_data = '!teach'},
+		}
+	}
+    return keyboard
+end
+local function do_keyboard_private3()
+    local keyboard = {}
+    keyboard.inline_keyboard = {
+    	{
+			{text = 'Ø¯Ø§Ù†Ø´ Ø¢Ù…ÙˆØ²', callback_data = '!school'},
+		},
+		{
+			{text = 'Ø¢Ù…ÙˆØ²Ú¯Ø§Ø±', callback_data = '!teach'},
+		}
+	}
+    return keyboard
+end
 local function do_keyboard_school()
-    local keyboard = {}
-    keyboard.inline_keyboard = {
-    	{{text = '?????? ?????? ???', callback_data = '!school1'}}, {{text = '?????? ?????? ???', callback_data = '!school2'}}}
-    return keyboard
+    local keyboard = {}
+    keyboard.inline_keyboard = {
+    	{{text = 'ğŸ“Ø¯ÙˆØ±Ù‡ Ù…ØªÙˆØ³Ø·Ù‡ Ø§ÙˆÙ„', callback_data = '!school1'}}, {{text = 'ğŸ“Ø¯ÙˆØ±Ù‡ Ù…ØªÙˆØ³Ø·Ù‡ Ø¯ÙˆÙ…', callback_data = '!school2'}}}
+    return keyboard
 end
 
 local function do_keyboard_teach()
-    local keyboard = {}
-    keyboard.inline_keyboard = {
-    	{{text = '?????? ?????? ???', callback_data = '!teach1'}}, {{text = '?????? ?????? ???', callback_data = '!teach2'}}}
-    return keyboard
+    local keyboard = {}
+    keyboard.inline_keyboard = {
+    	{{text = 'ğŸ“Ø¯ÙˆØ±Ù‡ Ù…ØªÙˆØ³Ø·Ù‡ Ø§ÙˆÙ„', callback_data = '!teach1'}}, {{text = 'ğŸ“Ø¯ÙˆØ±Ù‡ Ù…ØªÙˆØ³Ø·Ù‡ Ø¯ÙˆÙ…', callback_data = '!teach2'}}}
+    return keyboard
 end
 
 local function do_keyboard_school1()
-    local keyboard = {}
-    keyboard.inline_keyboard = {
-    	{{text = '??????', callback_data = '!school7'}},
-	{{text = '??????', url = '!school8'}},
-        {{text = '?????', url = '!school9'}},
-        {{text = '??', callback_data = '!school'}}
-    }
-    return keyboard
+    local keyboard = {}
+    keyboard.inline_keyboard = {
+    	{{text = 'ğŸ“šÙ‡ÙØªÙ…', callback_data = '!school7'}},
+	{{text = 'ğŸ“šÙ‡Ø´ØªÙ…', url = '!school8'}},
+        {{text = 'ğŸ“šÙ†Ù‡Ù…', url = '!school9'}},
+        {{text = 'ğŸ”™', callback_data = '!school'}}
+    }
+    return keyboard
 end
 
 local function do_keyboard_school2()
-    local keyboard = {}
-    keyboard.inline_keyboard = {
-    	{{text = '?????', callback_data = '!school10'}},
-	{{text = '????????', url = '!school11'}},
-        {{text = '?????????', url = '!school12'}},
-        {{text = '??', callback_data = '!school'}}
-    }
-    return keyboard
+    local keyboard = {}
+    keyboard.inline_keyboard = {
+    	{{text = 'ğŸ“šØ¯Ù‡Ù…', callback_data = '!school10'}},
+	{{text = 'ğŸ“šÛŒØ§Ø²Ø¯Ù‡Ù…', url = '!school11'}},
+        {{text = 'ğŸ“šØ¯ÙˆØ§Ø²Ø¯Ù‡Ù…', url = '!school12'}},
+        {{text = 'ğŸ”™', callback_data = '!school'}}
+    }
+    return keyboard
 end
 local action = function(msg, blocks, ln)
-local db10 = db:get('start:'..msg.from.id)
-local db11 = db:get('bot:general:users')
-local db12 = db:get(msg.from.id..':type')
-local stats = tonumber(db11) + 1
-    if blocks[1] == 'start' and not db10 then
-    db:set('start:'..msg.from.id,'true')
-    db:set('bot:general:users',stats)
-        if msg.chat.type == 'private' then
-            local message = "?????? "..msg.from.print_name.."!\n???? ?????? ??? ?? ?????? ????:"
-            local keyboard = do_keyboard_private()
-            api.sendKeyboard(msg.from.id, message, keyboard, true)
-            end
-        return
-    end
-    if blocks[1] == 'start' and blocks[2] and not db12 then
-        if msg.chat.type == 'private' then
-            local message = "?????? "..msg.from.print_name.."!\n???? ?????? ??? ?? ?????? ????:"
-            local keyboard = do_keyboard_private()
-            api.sendKeyboard(msg.from.id, message, keyboard, true)
-            end
-        return
-    end
-    if msg.cb then
-        local query = blocks[1]
-        local msg_id = msg.message_id
-        local text
+local hbb1 = 'start:'..msg.from.id
+local hbb2 = 'users'
+local hbbb3 = msg.from.id..':type'
+local db10 = db:get(hbb1)
+local db11 = db:get(hbb2)
+local db12 = db:get(hbb3)
+local stats = db11+1
+    if blocks[1] == 'start' and msg.chat.type == 'private' then
+    local hbb4 = 'start:'..msg.from.id
+    local hbb5 = 'started'
+    local hbb6 = 'users'
+    db:set(hbb4,hbb5)
+    db:set(hbb6,stats)
+            local message = "ğŸ‘‹Ø³Ù„Ø§Ù…!\nØ¯ÙˆØ±Ù‡ ØªØ­ØµÛŒÙ„ÛŒ Ø®ÙˆØ¯ Ø±Ø§ Ø§Ù†ØªØ®Ø§Ø¨ Ú©Ù†ÛŒØ¯:"
+            local keyboard = do_keyboard_private()
+            api.sendKeyboard(msg.from.id, message, keyboard, true)
+	else if blocks[1] == 'start' and blocks[2] and not db12 and msg.chat.type == 'private' then
+            local message = "ğŸ‘‹Ø³Ù„Ø§Ù…!\nØ¯ÙˆØ±Ù‡ ØªØ­ØµÛŒÙ„ÛŒ Ø®ÙˆØ¯ Ø±Ø§ Ø§Ù†ØªØ®Ø§Ø¨ Ú©Ù†ÛŒØ¯:"
+            local keyboard = do_keyboard_private2()
+            api.sendKeyboard(msg.from.id, message, keyboard, true)
+            end
+	end
+	return
+	end
+local action = function(msg, blocks, ln)
+    if msg.cb then
+        local query = blocks[1]
+        local msg_id = msg.message_id
+        local text
 if query == 'home' then
-            local text = "?????? "..msg.from.print_name.."!\n???? *??? ??????* ??? ?? ?????? ????:"
-            local keyboard = do_keyboard_private()
-        api.editMessageText(msg.chat.id, msg_id, text, keyboard, true)
+            local text = "ğŸ‘‹Ø³Ù„Ø§Ù…!\nÙ„Ø·ÙØ§ *Ù†ÙˆØ¹ Ú©Ø§Ø±Ø¨Ø±ÛŒ* Ø®ÙˆØ¯ Ø±Ø§ Ø§Ù†ØªØ®Ø§Ø¨ Ú©Ù†ÛŒØ¯:"
+            local keyboard = do_keyboard_private3()
+        api.editMessageText(msg.chat.id, msg_id, text, keyboard, true)
 end
 		if query == 'school' then
-            local text = "???? ??????* ??? ?? ?????? ????*:"
-            local keyboard = do_keyboard_school()
-        api.editMessageText(msg.chat.id, msg_id, text, keyboard, true)
+            local text = "Ø¯ÙˆØ±Ù‡ ØªØ­ØµÛŒÙ„ÛŒ* Ø®ÙˆØ¯ Ø±Ø§ Ø§Ù†ØªØ®Ø§Ø¨ Ú©Ù†ÛŒØ¯*:"
+            local keyboard = do_keyboard_school()
+        api.editMessageText(msg.chat.id, msg_id, text, keyboard, true)
 end
 if query == 'school1' then
-        local text = "???? ??????* ??? ?? ?????? ????*:"
-        local keyboard = do_keyboard_school1()
-    api.editMessageText(msg.chat.id, msg_id, text, keyboard, true)
+        local text = "Ù¾Ø§ÛŒÙ‡ ØªØ­ØµÛŒÙ„ÛŒ* Ø®ÙˆØ¯ Ø±Ø§ Ø§Ù†ØªØ®Ø§Ø¨ Ú©Ù†ÛŒØ¯*:"
+        local keyboard = do_keyboard_school1()
+    api.editMessageText(msg.chat.id, msg_id, text, keyboard, true)
 end
 if query == 'school2' then
-        local text = "???? ??????* ??? ?? ?????? ????*:"
-        local keyboard = do_keyboard_school2()
-    api.editMessageText(msg.chat.id, msg_id, text, keyboard, true)
+        local text = "Ù¾Ø§ÛŒÙ‡ ØªØ­ØµÛŒÙ„ÛŒ* Ø®ÙˆØ¯ Ø±Ø§ Ø§Ù†ØªØ®Ø§Ø¨ Ú©Ù†ÛŒØ¯*:"
+        local keyboard = do_keyboard_school2()
+    api.editMessageText(msg.chat.id, msg_id, text, keyboard, true)
 end
-	       if query == 'teach' then
-            local text = "???? ??????* ??? ?? ?????? ????*:"
-            local keyboard = do_keyboard_teach()
-        api.editMessageText(msg.chat.id, msg_id, text, keyboard, true)
-    end
-    end
+	       if query == 'teach' then
+            local text = "Ø¯ÙˆØ±Ù‡ ØªØ¯Ø±ÛŒØ³ÛŒ* Ø®ÙˆØ¯ Ø±Ø§ Ø§Ù†ØªØ®Ø§Ø¨ Ú©Ù†ÛŒØ¯*:"
+            local keyboard = do_keyboard_teach()
+        api.editMessageText(msg.chat.id, msg_id, text, keyboard, true)
+    end
+    end
 
 end
 
 return {
 	action = action,
 	triggers = {
-	    '^/(start)$',
-	    '^/(start) (.*)$',
-	    '^###cb:!(home)',
-	    '^###cb:!(school1)',
-	    '^###cb:!(school2)',
-	    '^###cb:!(teach)',
-	    '^###cb:!(school)',
+	    '^/(start)$',
+	    '^/(start) (.*)$',
+	    '^###cb:!(home)',
+	    '^###cb:!(school1)',
+	    '^###cb:!(school2)',
+	    '^###cb:!(teach)',
+	    '^###cb:!(school)',
 
 }}
